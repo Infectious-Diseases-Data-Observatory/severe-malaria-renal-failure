76c419942f48c36bf3631e82f12e4fc037b30ca2
jwatowatson <jwatowatson@gmail.com>
2025-07-01 08:08:32 +0100

    bicarbonate-base excess analysis

--- multiple_imputation.R
+++ multiple_imputation.R
@@ -5,6 +5,10 @@ library(missForest)
 library(doParallel)
 library(tictoc)
 library(naniar)
+library(mice)
+library(doParallel)
+library(foreach)
+
 
 
 RUN_IMPUTE_MODEL = T
@@ -17,33 +21,81 @@ key_cols=c("STUDY","COUNTRY","SITEID","SEX","Age","Died",
            "Coma_Final","Platelets_10_9_L",
            "Respiratory distress","Shock","Hypoglycaemia","Seizure","Jaundice",
            "Anemia","Hyperparasitaemia",
-           "para_ul","Glucose_mmol_L","Hb",
-           "HCT", "Lactate","Base Excess_mmol_L",
+           "para_ul","Glucose_mmol_L","Hb","HCT",
+           "Lactate","Base Excess_mmol_L","Bicarbonate_mEq_L",'pH_',
            "Creatinine_umol_L","BUN")
-sm_data = sm_data[, c('USUBJID', key_cols) ] %>%
+sm_data = sm_data[, key_cols ] %>%
   mutate(across(c(STUDY,COUNTRY,SITEID,SEX), as.factor),
          across(c(`Respiratory distress`,Shock,Hypoglycaemia,Seizure,
-                  Jaundice,Hyperparasitaemia,Anemia), as.numeric))%>%
+                  Jaundice,Hyperparasitaemia,Anemia), as.numeric),
+         log10_BUN = log10(BUN),
+         log10_Creatinine = log10(Creatinine_umol_L),
+         log10_Platelets = log10(Platelets_10_9_L),)%>%
   arrange(STUDY)
-sm_data = as.data.frame(sm_data)
-str(sm_data)
-vis_miss(sm_data %>% select(all_of(key_cols)) )
-summary(sm_data)
-
+# sm_data = as.data.frame(sm_data)
+# str(sm_data)
+# vis_miss(sm_data %>% select(all_of(key_cols)) )
+# summary(sm_data)
+#
 
 registerDoParallel(cores=6)
 
-test_indices = sample(1:nrow(sm_data),size = 5000,replace = F)
+
+n_cores <- parallel::detectCores() - 1  # Leave one core free
+cl <- makeCluster(n_cores)
+registerDoParallel(cl)
+
+# Number of imputations
+m <- 5
+test_indices = sample(1:nrow(sm_data),size = 1000,replace = F)
+xmis = sm_data %>% select(-Platelets_10_9_L, -BUN, - Creatinine_umol_L,-HCT)
+# Perform imputations in parallel
+imp_list <- foreach(i = 1:m, .packages = "mice", .combine = ibind) %dopar% {
+  mice(xmis, m = 1, method='rf', seed = i, printFlag = FALSE, maxit = 15)
+}
+
+# Stop the cluster
+stopCluster(cl)
+
+
+completed_data <- complete(imp_list, action = 1)
+plot_x_y_imputed = function(xmis, completed_data, x, y){
+  # Logical vectors indicating which values were imputed
+  x_imp <- is.na(xmis[,x])
+  y_imp <- is.na(xmis[,y])
+completed_data$x = completed_data[,x]
+completed_data$y = completed_data[,y]
+
+# Create a combined indicator: "none", "x", "y", or "both"
+  imp_status <- ifelse(x_imp & y_imp, "both",
+                       ifelse(x_imp, "x",
+                              ifelse(y_imp, "y", "none")))
+
+  # Add this to the completed data
+  completed_data$imp_status <- factor(imp_status, levels = c("none", "x", "y", "both"))
+  completed_data %>% ggplot(aes(x = x, y = y, color = imp_status)) +
+    geom_point(alpha = 0.5, size = 1.5) +
+    scale_color_manual(values = c("none" = "black", "x" = "blue", "y" = "red", "both" = "purple")) +
+    labs(color = "Imputation status") +
+    theme_minimal()
+}
+plot_x_y_imputed(xmis = xmis, completed_data = completed_data,
+                 x = 'Bicarbonate_mEq_L',y = "Base Excess_mmol_L")
 
 f_name = 'RData/imputed_dataset.RData'
-if(RUN_IMPUTE_MODEL){
-  tic();
-  sm_impute = missForest(xmis = sm_data[, -1], parallelize = 'forests',ntree = 100);
-  toc();
-  save(sm_impute, file = f_name)
-} else {
-  load(f_name)
-}
+save(imp_list, file = f_name)
+
+
+# if(RUN_IMPUTE_MODEL){
+#   tic();
+#   sm_impute = missForest(, parallelize = 'forests',ntree = 100);
+#   imp = mice(sm_data, m=2, method = 'rf', maxit=10, printFlag=TRUE)
+#
+#   toc();
+#   save(sm_impute, file = f_name)
+# } else {
+#   load(f_name)
+# }
 
 
 x_imp = sm_impute$ximp
